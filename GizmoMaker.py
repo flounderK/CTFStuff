633bfd6e5f657877cea39a987ea127d0db890e4f
Clif Wolfe <clifwolfe1@gmail.com>
2019-03-22 00:49:09 -0400

    Made an abomination

--- GizmoMaker.py
+++ GizmoMaker.py
@@ -27,8 +27,38 @@ class GizmoMaker:
             queue = [i if i != parameters[arg_ind] else args[arg_ind] for i in queue]
 
         payload = b''
+        if self.actions[name].get("Sub-Execution") is not None and self.actions[name]["Sub-Execution"]["Pre"] > 0:
+            payload += self.sub_execute(self.actions[name]["Sub-Execution"]["Pre"],
+                                        self.actions[name]["Sub-Execution"])
+
         for item in queue:
             payload += self.packer(item)
+
+        if self.actions[name].get("Sub-Execution") is not None and self.actions[name]["Sub-Execution"]["Post"] > 0:
+            payload += self.sub_execute(self.actions[name]["Sub-Execution"]["Post"],
+                                        self.actions[name]["Sub-Execution"])
+
+        return payload
+
+    def sub_execute(self, iters, specs: dict):
+        """This function will service as an easy way to perform other actions before or after this action is executed
+        as well as make small modifications to the parameters of the """
+        changing_param_ind = None
+
+        if specs.get("actionable_param") is not None and specs["actionable_param"] != "":
+            changing_param_ind = self.actions[specs["name"]]["Parameters"].index(specs["actionable_param"])
+
+        args = specs["args"]
+        payload = b''
+        for iteration in range(0, iters):
+
+            payload += self.execute(specs["name"], *args)
+            if changing_param_ind is not None:
+                args = list(args)
+                action_on_iteration = specs["action_on_iteration"]
+                args[changing_param_ind] = action_on_iteration(args[changing_param_ind])
+                args = tuple(args)
+
         return payload
 
     def init_template(self, name, *args):
@@ -47,7 +77,13 @@ class GizmoMaker:
         return {"name": "",
                 "Constants": list(),
                 "Parameters": list(),
-                "Queue": list()}
+                "Queue": list(),
+                "Sub-Execution": {"name": "",
+                                  "args": tuple(),
+                                  "Pre": 0,
+                                  "Post": 0,
+                                  "action_on_iteration": lambda a: a + 1,
+                                  "actionable_param": ""}}
 
     def load_templates(self, json_path):
         """Load templates from a json file. Structure of json should be [template, template, etc...]"""
@@ -69,5 +105,27 @@ def example_usage():
     print(gizmo_maker.execute("test_action", 0x4a4a4a4a, 0x4b4b4b4b))
 
 
+def test_sub():
+    gizmo_maker = GizmoMaker()
+    basic_template = gizmo_maker.get_template_template()
+    basic_template["name"] = "basic"
+    basic_template["Parameters"] = ["1", "2"]
+    basic_template["Queue"] = ["1", "2"]
+
+    template = gizmo_maker.get_template_template()
+    template["name"] = "adv"
+    template["Parameters"] = ["Param1", "Param2"]
+    template["Queue"] = ["Param1", "Param2"]
+    template["Sub-Execution"]["name"] = "basic"
+    template["Sub-Execution"]["Pre"] = 2
+    template["Sub-Execution"]["actionable_param"] = "1"
+    template["Sub-Execution"]["args"] = (0x1, 0x1)
+
+    gizmo_maker.add_template(basic_template)
+    gizmo_maker.add_template(template)
+
+    print(gizmo_maker.execute("adv", 0x1, 0x1))
 
 
+if __name__ == "__main__":
+    test_sub()
